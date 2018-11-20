345e7c6a931a410122ec34842f4ff9f519f21c97
Richard Ramos <info@richardramos.me>
2018-11-20 11:42:02 -0400

    Initial version

---
+++ app/js/index.js
@@ -0,0 +1,68 @@
+import EmbarkJS from 'Embark/EmbarkJS';
+
+const DEFAULT_CHANNEL = "default";
+
+EmbarkJS.onReady((err) => {
+    if(err){
+        alert("EmbarkJS is not available");
+        return;
+    }
+
+    const channelName = document.getElementById('channel-name')
+    channelName.innerHTML = DEFAULT_CHANNEL;
+
+    document.getElementById('chat-form').onsubmit = (e) => {
+        e.preventDefault();
+       
+        const message = document.getElementById('input-text').value;
+
+        // Send message via whisper
+        EmbarkJS.Messages.sendMessage({
+            topic: DEFAULT_CHANNEL, 
+            data: message
+        });
+    }
+
+
+    // Subscribe to whisper messages
+    EmbarkJS.Messages.listenTo({topic: [DEFAULT_CHANNEL]}, (error, message) => {
+        if(error){
+            alert("Error during subscription");
+            return;
+        }
+
+        const {data, time} = message; 
+        addMessage(data, time);
+    });
+
+
+    const addMessage = (data, time) => {
+        // Create new li for chat text 
+        const li = document.createElement('li');
+        const timeSpan = document.createElement("span");
+        const p = document.createElement("p");
+
+        const timeFormat = (new Date(time * 1000).toLocaleTimeString());
+
+        const attr = document.createAttribute("class"); 
+        attr.value = "time"; 
+
+        timeSpan.append(document.createTextNode(timeFormat));
+        timeSpan.setAttributeNode(attr);  
+        
+        p.appendChild(document.createTextNode(data));
+
+        li.appendChild(timeSpan);
+        li.appendChild(p);
+
+        const chatText = document.getElementById('chat-text');
+        chatText.appendChild(li);
+
+        // Scroll div
+        const chatArea = document.getElementById("chat-area");
+        chatArea.scrollTop = chatArea.scrollHeight;
+
+        // Clear text area
+        document.getElementById('input-text').value = "";
+    }
+});
